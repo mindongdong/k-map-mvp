a014a08030c63d9a615b6b4cbae7ba8b70b56c9f
Bcyan77 <paperosj2@gmail.com>
2025-12-29 15:47:07 +0900

    chore: add SQL dump files with Git LFS

---
+++ backend/scripts/sc_data_converted.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2a331085035bcc082850ea15e131b5aa295c78c41612de5897aa51f6fe0bf5de
+size 346482523
---
+++ backend/scripts/sc_data_dump.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f982b8e5a6ecb076104cb6548a64b197d34075c8996f87aa17805b45b7fb3ad2
+size 345937730
